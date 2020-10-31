cda2b22ddf799a7a18573bedf67d382924528a8e
irfansofyana <irfansofyana0305@gmail.com>
2020-10-31 14:42:39 +0700

    Change paradigm into OOP

--- src/algorithm/elgamal.py
+++ src/algorithm/elgamal.py
@@ -3,101 +3,108 @@ from src.utils.file import *
 import random
 import math
 
-def encode(plaintext, num_bits):
-    bytes_array = bytearray(plaintext, 'utf-16')
-    encoded = []
-    byte_size = num_bits // 8
-    idx_encoded = -byte_size
-
-    for i in range(len(bytes_array)):
-        if (i % byte_size == 0):
-            idx_encoded += byte_size
-            encoded.append(0)
-        val = (2 ** (8 * (i % byte_size)))
-        encoded[idx_encoded//byte_size] += bytes_array[i] * val
-
-    return encoded
-
-def decode(plaintext, num_bits):
-    bytes_array = []
-    byte_size = num_bits // 8
-
-    for num in plaintext:
-        for i in range(byte_size):
-            temp = num
-            for j in range(i+1, byte_size):
-                temp = temp % (2 ** (8 * j))
-            letter = temp // 2 ** (8 * i)
-            bytes_array.append(letter)
-            num -= (letter * (2 ** (8 * i)))
+class Elgamal():
+    def __init__(self, num_bits):
+        self.num_bits = num_bits
+        self.key = self.generate_key(self.num_bits)
     
-    decoded_text = bytearray(b for b in bytes_array).decode('utf-16')
-    return decoded_text
+    def encode(self, plaintext):
+        bytes_array = bytearray(plaintext, 'utf-16')
+        encoded = []
+        byte_size = self.num_bits // 8
+        idx_encoded = -byte_size
 
-def generate_key(num_bits=256):
-    p = getPrimeNbit(num_bits)
-    g = random.randint(1, p-1)
-    x = random.randint(1, p-2)
-    y = powmod(g, x, p)
+        for i in range(len(bytes_array)):
+            if (i % byte_size == 0):
+                idx_encoded += byte_size
+                encoded.append(0)
+            val = (2 ** (8 * (i % byte_size)))
+            encoded[idx_encoded//byte_size] += bytes_array[i] * val
 
-    public_key = { 'y' : y, 'g' : g, 'p' : p }
-    private_key = { 'x' : x, 'p' : p }
+        return encoded
 
-    return {'public': public_key, 'private': private_key}
+    def decode(self, plaintext):
+        bytes_array = []
+        byte_size = self.num_bits // 8
 
-def encrypt(plaintext, key, num_bits):
-    encoded = encode(plaintext, num_bits)
-    ciphers = []
-    y, g, p = key.values()
+        for num in plaintext:
+            for i in range(byte_size):
+                temp = num
+                for j in range(i+1, byte_size):
+                    temp = temp % (2 ** (8 * j))
+                letter = temp // 2 ** (8 * i)
+                bytes_array.append(letter)
+                num -= (letter * (2 ** (8 * i)))
+        
+        decoded_text = bytearray(b for b in bytes_array).decode('utf-16')
+        return decoded_text
 
-    for val in encoded:
-        k = random.randint(1, p-2)
-        a = powmod(g, k, p)
-        b = ((val % p) * powmod(y, k, p)) % p
-        ciphers.append([a, b])
-    
-    encrypted_str = ""
-    for cipher in ciphers:
-        encrypted_str += str(cipher[0]) + ' ' + str(cipher[1]) + ' '
-    
-    return encrypted_str
+    def generate_key(self, num_bits=256):
+        p = getPrimeNbit(num_bits)
+        g = random.randint(1, p-1)
+        x = random.randint(1, p-2)
+        y = powmod(g, x, p)
 
-def decrypt(ciphertext, key, num_bits):
-    plaintext = []
-    ciphers_array = ciphertext.split()
-    x, p = key.values()
+        public_key = { 'y' : y, 'g' : g, 'p' : p }
+        private_key = { 'x' : x, 'p' : p }
 
-    if (not len(ciphers_array) % 2 == 0):
-        return "BAD Ciphertext found!, can't decrypt it!"
-    
-    for i in range(0, len(ciphers_array), 2):
-        a = int(ciphers_array[i])
-        b = int(ciphers_array[i + 1])
-        a = powmod(a, x, p)
-        plain = (b * powmod(a, p-2, p)) % p
-        plaintext.append(plain)
-    
-    decrypted_str = decode(plaintext, num_bits)
-    decrypted_str = "".join([ch for ch in decrypted_str if (ch != '\x00')])
+        return {'public': public_key, 'private': private_key}
 
-    return decrypted_str
+    def encrypt(self, plaintext):
+        encoded = self.encode(plaintext)
+        ciphers = []
+        y, g, p = self.key['public'].values()
 
-def save_key(key, is_public, filename):
-    filename += '.pub' if is_public else '.pri'
-    key_type = 'public' if is_public else 'private'
+        for val in encoded:
+            k = random.randint(1, p-2)
+            a = powmod(g, k, p)
+            b = ((val % p) * powmod(y, k, p)) % p
+            ciphers.append([a, b])
+        
+        encrypted_str = ""
+        for cipher in ciphers:
+            encrypted_str += str(cipher[0]) + ' ' + str(cipher[1]) + ' '
+        
+        return encrypted_str
 
-    write_file(filename, key[key_type]) 
+    def decrypt(self, ciphertext):
+        plaintext = []
+        ciphers_array = ciphertext.split()
+        x, p = self.key['private'].values()
+
+        if (not len(ciphers_array) % 2 == 0):
+            return "BAD Ciphertext found!, can't decrypt it!"
+        
+        for i in range(0, len(ciphers_array), 2):
+            a = int(ciphers_array[i])
+            b = int(ciphers_array[i + 1])
+            a = powmod(a, x, p)
+            plain = (b * powmod(a, p-2, p)) % p
+            plaintext.append(plain)
+        
+        decrypted_str = self.decode(plaintext)
+        decrypted_str = "".join([ch for ch in decrypted_str if (ch != '\x00')])
+
+        return decrypted_str
+
+    def save_key(self, is_public, filename):
+        filename += '.pub' if is_public else '.pri'
+        key_type = 'public' if is_public else 'private'
+
+        write_file(filename, self.key[key_type]) 
 
 if (__name__=="__main__"):
     # plaintext = "this is only a plaintext for testing encode and decode"
-    # encoded = encode(plaintext, 256)
+    # elgamal = Elgamal(num_bits = 256)
+    # encoded = elgamal.encode(plaintext)
     # print(encoded)
-    # decoded = decode(encoded, 256)
+    # decoded = elgamal.decode(encoded)
     # print(decoded)
 
-    keys = generate_key(num_bits=256)
-    plaintext = "irfan sofyana putra adalah orang yang mengerjakan tugas bagian ini gan. Duh kok masih ada tugas padahal long weekend"
-    encrypted = encrypt(plaintext, keys['public'], num_bits=256)
-    print(encrypted)
-    decrypted = decrypt(encrypted, keys['private'], num_bits=256)
-    print(decrypted)
+    # elgamal = Elgamal(num_bits = 256)
+    # plaintext = "irfan sofyana putra adalah orang yang mengerjakan tugas bagian ini gan. Duh kok masih ada tugas padahal long weekend"
+    # encrypted = elgamal.encrypt(plaintext)
+
+    # print(encrypted)
+    # decrypted = elgamal.decrypt(encrypted)
+    # print(decrypted)
